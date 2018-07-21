28e82302d7853375c0ae377ba64c7cfa4c66362f
Hyeon Woo Jeong <qlgks1@naver.com>
2018-07-21 23:26:26 +0900

    create HyeonWoo dir (git test)

---
+++ HyeonWoo_test/SearchingTestProject/config/config.php
@@ -0,0 +1,8 @@
+<?php
+	$config = array(
+	"host"=>"localhost:3306",
+	"duser"=>"root",
+	"dpw"=>"123456789",
+	"dname"=>"searching"
+	);
+?>
---
+++ HyeonWoo_test/SearchingTestProject/lib/db.php
@@ -0,0 +1,8 @@
+<?php
+	function db_init($host, $duser, $dpw, $dname)
+	{
+		$conn = mysqli_connect($host,$duser,$dpw);
+		mysqli_select_db($conn, $dname);
+		return $conn;
+	}
+?>
