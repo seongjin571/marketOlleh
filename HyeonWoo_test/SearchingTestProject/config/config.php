25c6103290a17652ada7f5cd498d1f0c3d524def
Hyeon Woo Jeong <qlgks1@naver.com>
2018-07-22 14:36:42 +0900

    remove HyeonWoo dir

--- HyeonWoo_test/SearchingTestProject/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$config = array(
-	"host"=>"localhost:3306",
-	"duser"=>"root",
-	"dpw"=>"123456789",
-	"dname"=>"searching"
-	);
-?>
--- HyeonWoo_test/SearchingTestProject/lib/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function db_init($host, $duser, $dpw, $dname)
-	{
-		$conn = mysqli_connect($host,$duser,$dpw);
-		mysqli_select_db($conn, $dname);
-		return $conn;
-	}
-?>
